df608cf8f87a57e0785e61fbfe48e426d69808a7
benbeedessy15@gmail.com <benbeedessy15@gmail.com>
2024-05-03 11:56:44 +0400

    day one

--- week 3/day four/exe/index.py
+++ week 3/day four/exe/index.py
@@ -1,4 +1,13 @@
 import json
 
-with open('week 3\day four\exe\file.json', 'r') as file:
+
+# Read the contents of the file
+with open('file.json', 'r') as file:
     data = json.load(file)
+
+# Perform operations on the data
+# ...
+
+# Write the updated data back to the file
+with open('file.json', 'w') as file:
+    json.dump(data, file)
---
+++ week1/day1/day one/readme.md
@@ -0,0 +1,3 @@
+sbdcndcsd
+
+gfhj
